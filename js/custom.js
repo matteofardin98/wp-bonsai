737f0349e35e1304d14aeebfe88c6725dbd70fc0
matteofardin98 <teo.fardi98@gmail.com>
2024-01-31 16:29:27 +0100

    Creazione git progetto wp-bonsai

---
+++ js/custom.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+        //Burger
+        $('.burger-menu').click(function () {
+            $('nav').toggleClass('wide');
+        });
+        
+        //Scroll color header
+        $(window).scroll(function () {
+            var scrollPosition = $(this).scrollTop();
+    
+            if (scrollPosition > 100) {
+                $('header').addClass('scrolled');
+            } else {
+                $('header').removeClass('scrolled');
+            }
+        });
+    
+        // Scroll Top
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 100) {
+                $('.scroll-top').fadeIn();
+            } else {
+                $('.scroll-top').fadeOut();
+            }
+        });
+
+});
+
+// Burger Transition in JavaScript
+document.getElementById('burgerMenu').addEventListener('click', function () {
+    this.classList.toggle('active');
+});
+
